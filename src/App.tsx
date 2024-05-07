41fa6d8211dbfaa08385873630932528da268904
lloydagola <lloydagola@users.noreply.github.com>
2024-05-07 23:55:06 +0300

    refaactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Main from './templates/Main';
+import Main from './layouts/Main';
 
 function App() {
   return (
