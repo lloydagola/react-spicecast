b59ba49f6ddbd660a36a432bec8c2ec45c93eb60
lloydagola <lloydagola@users.noreply.github.com>
2024-05-08 00:30:25 +0300

    refactoring

--- src/components/audioPlayer/AudioPlayer.tsx
+++ src/components/audioPlayer/AudioPlayer.tsx
@@ -1,8 +1,10 @@
 export function AudioPlayer() {
 
-    return <div> <figure>
-  <figcaption>Listen to the T-Rex:</figcaption>
-  <audio controls src="./chill.mp3"></audio>
-  <a href="/media/cc0-audio/t-rex-roar.mp3"> Download audio </a>
-</figure></div>;
+    return <div> 
+      <figure>
+        <figcaption>Listen to the T-Rex:</figcaption>
+        <audio controls src="./chill.mp3"></audio>
+        <a href="/media/cc0-audio/t-rex-roar.mp3"> Download audio </a>
+      </figure> 
+    </div>;
 }
