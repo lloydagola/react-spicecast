1476b907bfeb0f805738b7d45617988b2f5b0782
lloydagola <lloydagola@users.noreply.github.com>
2024-05-08 00:42:11 +0300

    added audio player style

--- src/components/audioPlayer/AudioPlayer.tsx
+++ src/components/audioPlayer/AudioPlayer.tsx
@@ -1,10 +1,40 @@
+const audioPlayerStyle = {
+    backgroundColor: '#333',
+    border: '1px solid #333',
+    display: 'flex',
+    position: 'fixed',
+    bottom: '0',
+    zIndex: '10000000',
+    cursor: 'pointer',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '90%',
+    color: '#fff',
+    padding: '0 5%',
+    caretColor: 'transparent',    
+}
+
 export function AudioPlayer() {
 
-    return <div> 
+    return <section style={{
+    backgroundColor: '#333',
+    border: '1px solid #333',
+    display: 'flex',
+    position: 'fixed',
+    bottom: '0',
+    zIndex: '10000000',
+    cursor: 'pointer',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '90%',
+    color: '#fff',
+    padding: '0 5%',
+    caretColor: 'transparent',    
+}}> 
       <figure>
         <figcaption>Listen to the T-Rex:</figcaption>
-        <audio controls src="./chill.mp3"></audio>
+        <audio controls src="./chill.mp3" ></audio>
         <a href="/media/cc0-audio/t-rex-roar.mp3"> Download audio </a>
       </figure> 
-    </div>;
+    </section>;
 }
