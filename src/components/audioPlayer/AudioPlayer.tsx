2fe5fce18f1c16759898a0dcf3f642afa2066a52
lloydagola <lloydagola@users.noreply.github.com>
2024-05-08 16:33:34 +0300

    refactored style

--- src/components/audioPlayer/AudioPlayer.tsx
+++ src/components/audioPlayer/AudioPlayer.tsx
@@ -1,13 +1,76 @@
+import styled from '@emotion/styled';
+
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import StopIcon from '@mui/icons-material/Stop';
 
-import './styles.css';
+const StyledAudioPlayer = styled.section(({ theme }) => ({
+    backgroundColor: "#333",
+    border: "1px solid #333",
+    display: "flex",
+    position: "fixed",
+    bottom: "0",
+    zIndex: "10000000",
+    cursor: "pointer",
+    alignItems: "center",
+    flexDirection: "row",
+    color: "#fff",
+    padding: "0 5%",
+    caretColor: "transparent",
+    width:'100%',
 
-export default function AudioPlayer(){
+    'img':{
+        height: "60px",
+        width: "60px",
+        borderRadius: "48%",
+        backgroundSize: "cover",
+        margin: "4px",
+    },
 
-    return <section id="audio-player" className="audio-player">   
+    'progress':{
+        width: "50%",
+        margin: "0 16px",
+        border: "none",
+        WebkitAppearance:'none',
+        appearance: "none",
+        backgroundColor: "white",
+        color: "blue",
+        height: "2px",
+    },
+
+    'p':{
+        fontSize:'1.2rem'
+    },
+   
+}));
+
+const StyledPlayerControls = styled.div(({theme}) => ({
+    display: "grid",
+    gridTemplateColumns:" repeat(4, 1fr)",
+    alignItems: "center",
+    width: "250px",
+}))
+
+const StyledTrackTitle = styled.div(() => ({
+    width: "400px",
+    margin: "0 20px",
+    textOverflow: "ellipsis",
+    whiteSpace:"nowrap",
+    overflow: "hidden",   
+    display: "block", 
+
+     'h4':{
+        textOverflow: "ellipsis",
+        overflow: "hidden",
+        margin: "0",
+    }
+}))
+
+
+function AudioPlayer():JSX.Element{
+
+    return <StyledAudioPlayer id="audio-player">   
                 <audio >
                     Your browser does not support the
                     <code>audio</code> element.
@@ -15,21 +78,22 @@ export default function AudioPlayer(){
                     <source src="http://localhost:4000/music/Xilent/we are dust/01 From Dust.mp3" type="audio/mpeg"/>
                 </audio>
                 <img src='https://avatars.githubusercontent.com/u/31162324?v=4' alt='thumb'/>
-                <div className="audio-title">
-                    <h4>audioState.nowPlaying.parent.title - audioState.nowPlaying.title</h4>
-                </div>
-                <div className="player-controls">
-                    <PlayCircleFilledIcon/>
-                    <SkipPreviousIcon/>
-                    <StopIcon/>
-                    <SkipNextIcon/>
-                </div>
-                <p>currentTime</p>
+                <StyledTrackTitle>
+                    <h4>Lloyd Aagola - Still Waiting</h4>
+                </StyledTrackTitle>
+                <StyledPlayerControls>
+                    <PlayCircleFilledIcon fontSize='large'/>
+                    <SkipPreviousIcon fontSize='large'/>
+                    <StopIcon fontSize='large'/>
+                    <SkipNextIcon fontSize='large'/>
+                </StyledPlayerControls>
+                <p>00:00</p>
                 <progress id="seek-obj" value=".75" max="1" />        
-                <p>trackDuration</p>
-            </section> 
-
+                <p>04:00</p>
+            </StyledAudioPlayer> 
 }
+
+export default AudioPlayer;
 
  
 
