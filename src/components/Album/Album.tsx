aa72238ad1777949aba012d55ebd0aa30cd5ffb7
lloydagola <lloydagola@users.noreply.github.com>
2024-05-08 20:00:26 +0300

    created Album scaffolding

---
+++ src/components/Album/Album.tsx
@@ -0,0 +1,21 @@
+import "./style.css";
+
+type TAlbumTypes = {
+    i:number
+}
+
+export default function Album({i}:TAlbumTypes)  {
+
+    return <div className="single-album"> 
+        <div className="album-art">
+            <div className="album-filter"/>
+            <img src={`./images/th-${i}.jpg`} alt='album thumbnail'/> 
+        </div>  
+        <div className="album-buttons"/>        
+        <div className = "album-text">
+            <h4>Title</h4>
+            <p>Lloyd Agola</p>
+            <div className="horizontal-line album-horizontal-line white-background"/>                                            
+        </div>  
+    </div>
+}
