a7c4a3d7e046b72a2fdd89d4b2824f166da06981
lloydagola <lloydagola@users.noreply.github.com>
2024-05-08 20:37:47 +0300

    added playback functionality

--- src/components/audioPlayer/AudioPlayer.tsx
+++ src/components/audioPlayer/AudioPlayer.tsx
@@ -1,27 +1,49 @@
 
+import { useRef } from 'react';
+
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import StopIcon from '@mui/icons-material/Stop';
 import { StyledAudioPlayer, StyledTrackTitle, StyledPlayerControls } from './AudioPlayer.styles';
 
+type TCurrent = {
+
+}
+
+
 export default function AudioPlayer():JSX.Element{
+    const player = useRef<HTMLAudioElement>(null);
+    
+    const handlePlay = () => {
+
+        if (player?.current?.paused === false) {
+            player?.current?.pause();
+            
+        }
+        else {
+            player?.current?.play();  
+        }
+    }
+
+     const handleStop = () => {
+        player?.current?.pause();
+    }
 
     return <StyledAudioPlayer id="audio-player">   
-                <audio >
+                <audio  ref={player}>
                     Your browser does not support the
                     <code>audio</code> element.
-                    <source src="${BASE_URL}/${audioState.nowPlaying.path}`}" type="audio/mpeg"/>
-                    <source src="http://localhost:4000/music/Xilent/we are dust/01 From Dust.mp3" type="audio/mpeg"/>
+                    <source src="./chill.mp3" type="audio/mpeg"/>
                 </audio>
-                <img src='https://avatars.githubusercontent.com/u/31162324?v=4' alt='thumb'/>
+                <img src='./images/th-15.jpg' alt='thumb'/>
                 <StyledTrackTitle>
                     <h4>Lloyd Aagola - Still Waiting</h4>
                 </StyledTrackTitle>
                 <StyledPlayerControls>
-                    <PlayCircleFilledIcon fontSize='large'/>
+                    <PlayCircleFilledIcon fontSize='large' onClick={() => handlePlay()}/>
                     <SkipPreviousIcon fontSize='large'/>
-                    <StopIcon fontSize='large'/>
+                    <StopIcon fontSize='large' onClick={() => handleStop()}/>
                     <SkipNextIcon fontSize='large'/>
                 </StyledPlayerControls>
                 <p>00:00</p>
