f102e46919a96e37449f092af2193056a6b7b4e4
lloydagola <lloydagola@users.noreply.github.com>
2024-05-10 00:41:55 +0300

    refactored playlistView styles

--- src/components/PlaylistView/PlaylistView.jsx
+++ src/components/PlaylistView/PlaylistView.jsx
@@ -1,15 +1,24 @@
+import Box from '@mui/material/Box'
 import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
-import Typography from '@mui/material/Typography/Typography'
-import './styles.css'; 
+import Typography from '@mui/material/Typography/Typography';
 
+const playListViewStyles = {
+    backgroundColor: '#fff',
+    overflowX: 'hidden',   
+    position: 'absolute',
+    zIndex: '100',
+    bottom: '0', 
+    marginBottom: '16px',
+    maxWidth: '80%', 
+}
 
 
 export default function PlaylistView (){    
 
-    return <div className="playlist-view">
-                <div className="podcast-play">
+    return <Box maxWidth='95%' className="playlist-view" sx={playListViewStyles}>
+                <Box className="podcast-play" display='flex' flexDirection='row' alignItems='center' p='4px'>
                     <PlayCircleFilledWhiteOutlinedIcon sx={{color:'#222'}}/>
-                    <Typography fontWeight={600} className="playing">Ep. 109</Typography>                      
-                </div>  
-            </div>
+                    <Typography fontWeight={600} className="playing" color="#333" overflow='hidden' textOverflow='ellipsis' whiteSpace='nowrap' p='8px'>Ep. 109</Typography>                      
+                </Box>  
+            </Box>
 };
