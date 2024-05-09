b1f38162ed26cc7860c12def6696caafca9df7d5
lloydagola <lloydagola@users.noreply.github.com>
2024-05-10 01:47:59 +0300

    ui polish

--- src/layouts/Body/Body.tsx
+++ src/layouts/Body/Body.tsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import Albums from 'src/components/Albums/Albums';
 import Podcasts from 'src/components/Podcasts/Podcasts';
+import RadioStations from 'src/components/RadioStations/RadioStations';
 
 
 export type TBodyTypes = {drawerWidth:number}
@@ -8,8 +9,9 @@ export type TBodyTypes = {drawerWidth:number}
 export default function Body({ drawerWidth }: TBodyTypes): JSX.Element {
     return <Box
                 component="main"
-                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+                sx={{ flexGrow: 1, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
             >               
+                <RadioStations/>
                 <Albums/>
                 <Podcasts/>
             </Box>;
