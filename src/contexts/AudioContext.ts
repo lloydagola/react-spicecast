ca6fefb10db6c61f6887e5faed28cf2399c19c9c
lloydagola <lloydagola@users.noreply.github.com>
2024-05-14 16:59:41 +0300

    created context

--- src/contexts/AudioContext.ts
+++ src/contexts/AudioContext.ts
@@ -2,7 +2,26 @@ import { createContext } from "react";
 import { TAudioContext } from "src/types/types";
 
 
-const AudioContext = createContext<TAudioContext>({radioStations:[], albums:[], podcasts:[]});
+const initialState ={     
+    audioData: {
+        audioState: { 
+                trackType: 'radioStation',
+                isPlaying: false,
+                nowPlaying:"LunarFM",
+                streamUrl:"http://208.64.184.36/sf1033",
+                thumbnail:"/images/th-2.jpg",
+                duration:""
+            }, 
+            radioStations:[], 
+            albums:[], 
+            podcasts:[]
+    },
+    handlePlay: function(){},
+    handlePause: function(){},
+    handleStop: function(){},
+}
+
+const AudioContext = createContext<TAudioContext>(initialState);
 const AudioContextProvider = AudioContext.Provider;
 
 export {AudioContext, AudioContextProvider};
