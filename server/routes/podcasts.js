105def5c5e3042b4e0b8bf4baa64755d1f127a02
lloydagola <lloydagola@users.noreply.github.com>
2024-05-28 22:56:26 +0300

    hardcoded data for mocking

--- server/routes/podcasts.js
+++ server/routes/podcasts.js
@@ -1,55 +1,53 @@
 const express = require("express");
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require("express-validator");
 const Podcast = require("../models/podcast");
+const test = require("./podcasts.json");
 
 const router = express.Router();
 
-router.get('/', (request, response) => {
-    Podcast
-    .find()
+router.get("/test", (request, response) => {
+  response.status(200).send(test);
+});
+
+router.get("/", (request, response) => {
+  Podcast.find()
     .populate("episodes")
     .populate("hosts", ["title"])
-    .then(podcasts => response.status(200).send(podcasts))
-    .catch(error => response.status(400).send(error));   
+    .then((podcasts) => response.status(200).send(podcasts))
+    .catch((error) => response.status(400).send(error));
 });
 
 router.get("/:_id", (request, response) => {
-    console.log(request.params._id);
+  console.log(request.params._id);
 
-    Podcast
-    .findById(request.params._id)
+  Podcast.findById(request.params._id)
     .populate("episodes")
     .populate("hosts", ["title"])
-    .then(podcast => {
-        response.status(200).send(podcast);
-    })
-    
-})
+    .then((podcast) => {
+      response.status(200).send(podcast);
+    });
+});
 
 router.post("/", (request, response) => {
+  const newPodcast = new Podcast(request.body);
 
-    const newPodcast = new Podcast(request.body)
-    
-    newPodcast
+  newPodcast
     .save()
-    .then(savedPodcast => response.status(200).send(savedPodcast))
-    .catch(error => response.status(400).send({error}))
+    .then((savedPodcast) => response.status(200).send(savedPodcast))
+    .catch((error) => response.status(400).send({ error }));
 });
 
 router.put("/:_id", (request, response) => {
-        
-    if(request.params._id && request.body.attribute && request.body.value){
-       
-            Podcast
-            .findByIdAndUpdate(request.params._id, {[request.body.attribute] : request.body.value})
-            .then(result => response.status(200).send(result))
-            .catch(error => response.status(400).send(error));
-       
-    }
-    else response.status(400).send({error: "attribute and it's corresponding value is required!"})
+  if (request.params._id && request.body.attribute && request.body.value) {
+    Podcast.findByIdAndUpdate(request.params._id, {
+      [request.body.attribute]: request.body.value,
+    })
+      .then((result) => response.status(200).send(result))
+      .catch((error) => response.status(400).send(error));
+  } else
+    response
+      .status(400)
+      .send({ error: "attribute and it's corresponding value is required!" });
 });
-
-
-   
 
 module.exports = router;
