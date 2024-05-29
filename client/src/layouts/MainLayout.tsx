816d79ab24fece6bbe41cc18181c6c5112fcc31f
lloydagola <lloydagola@users.noreply.github.com>
2024-05-30 01:23:48 +0300

    updated types

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -3,8 +3,17 @@ import { TopBar } from './components/TopBar/TopBar';
 import { drawerWidth } from 'src/utils/constants';
 import SidebarLeft from './components/SidebarLeft/SidebarLeft';
 import SidebarRight from './components/SidebarRight/SidebarRight';
+import { ReactNode, Dispatch, SetStateAction } from 'react';
 
-export default function MainLayout({children, setIsClosing,mobileOpen,setMobileOpen, handleDrawerToggle }:any) {
+type TMainLayoutProps = {
+  children: ReactNode[];
+  setIsClosing: Dispatch<SetStateAction<boolean>>;
+  mobileOpen: boolean;
+  setMobileOpen: Dispatch<SetStateAction<boolean>>;
+  handleDrawerToggle: () => void; 
+}
+
+export default function MainLayout({children, setIsClosing,mobileOpen,setMobileOpen, handleDrawerToggle }:TMainLayoutProps) {
   
   return (
     <Box sx={{ display: 'flex', flexDirection:'column',color:'#fff', 'header': {boxShadow:'1px solid #fff'} }} >     
