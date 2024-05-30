3d31048fcd36d5503146c7824fc18a16682be504
lloydagola <lloydagola@users.noreply.github.com>
2024-05-30 22:03:31 +0300

    ui polish

--- client/src/components/RadioStation/RadioStation.tsx
+++ client/src/components/RadioStation/RadioStation.tsx
@@ -31,8 +31,8 @@ const radioStationStyles = {
 
 export default function RadioStation ({radioStation}:TRadioStationProps): JSX.Element{
 
-    return <Grid item height="200px" alignItems='center' overflow='hidden' position='relative' sx={radioStationStyles}> 
-            <Box className="image-view" overflow='hidden' position='relative' height='100%'>
+    return <Grid item alignItems='center' overflow='hidden' position='relative' sx={radioStationStyles}> 
+            <Box className="image-view" overflow='hidden' position='relative' height='250px'>
                 <Box position='absolute' zIndex={100} mt={4} ml={1} >
                     <Typography variant='h4' fontSize={26} fontWeight={900} textOverflow='ellipsis' overflow='hidden'>{radioStation.title}</Typography>                    
                 </Box> 
