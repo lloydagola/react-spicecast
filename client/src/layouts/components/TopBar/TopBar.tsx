135622b5326222430ea63d9afbe95586ba4ef874
lloydagola <lloydagola@users.noreply.github.com>
2024-05-30 22:48:56 +0300

    implemented mobile menu icon

--- client/src/layouts/components/TopBar/TopBar.tsx
+++ client/src/layouts/components/TopBar/TopBar.tsx
@@ -14,10 +14,19 @@ import LocalActivityIcon from '@mui/icons-material/LocalActivity';
 import PodcastsIcon from '@mui/icons-material/Podcasts';
 import IconButton from '@mui/material/IconButton';
 import StreamIcon from '@mui/icons-material/Stream';
+import MenuIcon from '@mui/icons-material/Menu';
 
 import { drawerWidth } from 'src/utils/constants';
 import useDrawer from 'src/hooks/useDrawer';
 import useScrollPosition from 'src/hooks/useScrollPosition';
+import styled from '@emotion/styled';
+
+const StyledDevice  = styled(Box)(({theme})=>({
+  display:'none',
+  '@media (min-width:480px)': {
+    display:'flex',
+  },
+}))
 
 
 export function TopBar(): JSX.Element {
@@ -29,62 +38,78 @@ export function TopBar(): JSX.Element {
   return (
     <>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)`, md: '100%' },
-          ml: { sm: `${drawerWidth}px` },
-          zIndex: { md: '100000' },
-          backgroundColor: inViewport ? 'rgba(0,0,0,0.9)' : 'rgba(0,0,0,0)',
-          transition: '.2s ease-in-out',
-          boxShadow: inViewport ? '1px solid #fff' : 'unset'
-        }}
-      >
-        <Toolbar sx={{ justifyContent: 'center' }}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+        <AppBar
+            position="fixed"
+            sx={{
+              width: { sm: `calc(100% - ${drawerWidth}px)`, md: '100%' },
+              ml: { sm: `${drawerWidth}px` },
+              display: { lg: 'none' },
+              zIndex: { md: '100000' },
+              backgroundColor: inViewport ? 'rgba(0,0,0,0.9)' : 'rgba(0,0,0,0)',
+              transition: '.2s ease-in-out',
+              boxShadow: inViewport ? '1px solid #fff' : 'unset'
+            }}
           >
-          </IconButton>
-          <ListItemIcon><StreamIcon sx={{ color: '#fff', fontSize: '3rem' }} /></ListItemIcon>  
-          <NavLink to='/' style={{color:'#fff', textDecoration:'none'}}>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              marginRight='80px'
-              fontSize={36}
-              fontWeight={900}
-              sx={{
-                textShadow: '2px 2px 5px rgba(0,0,0,0.3), 5px 5px 70px rgba(255, 255, 255, 0.5)'
-              }}>
-              Spice Cast
-            </Typography>
-          </NavLink>        
-          <Box display='flex'>
-            <List sx={{ display: 'flex', flexDirection: 'row' }}>
-              {['Albums', 'Podcasts', 'Events', 'RadioStations'].map((text, index) => (
-                <NavLink to={`/${text.toLocaleLowerCase()}`} style={{color:'#fff', textDecoration:'none'}}  key={index}>
-                  <ListItem>
-                      <ListItemButton>                        
-                        <ListItemIcon>
-                          {text === 'Albums' ? <LibraryMusicIcon sx={{ color: '#fff', }} /> :
-                            text === 'Podcasts' ? <PodcastsIcon sx={{ color: '#fff' }} /> :
-                              text === 'Events' ? <LocalActivityIcon sx={{ color: '#fff' }} /> :
-                                <RadioIcon sx={{ color: '#fff' }} />}
-                        </ListItemIcon>
-                        <Typography fontSize={22} fontWeight={600} sx={{ textShadow: '2px 2px 5px rgba(0,0,0,0.3), 5px 5px 70px rgba(255, 255, 255, 0.5)' }}>{text}</Typography>            
-                      </ListItemButton>
-                    </ListItem>
-                </NavLink>
-              ))}
-            </List>
-          </Box>
-        </Toolbar>
-      </AppBar>
+          <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ ml:2, mr: 2, width:'max-content'}}
+            >
+                <MenuIcon sx={{fontWeight:900, fontSize:'2.5rem' }}/>
+            </IconButton>
+          </AppBar>
+        <StyledDevice>
+          <AppBar
+            position="fixed"
+            sx={{
+              width: { sm: `calc(100% - ${drawerWidth}px)`, md: '100%' },
+              ml: { sm: `${drawerWidth}px` },
+              zIndex: { md: '100000' },
+              backgroundColor: inViewport ? 'rgba(0,0,0,0.9)' : 'rgba(0,0,0,0)',
+              transition: '.2s ease-in-out',
+              boxShadow: inViewport ? '1px solid #fff' : 'unset'
+            }}
+          >
+            
+            <Toolbar sx={{ justifyContent: 'center' }}>
+            
+                <ListItemIcon><StreamIcon sx={{ color: '#fff', fontSize: '3rem' }} /></ListItemIcon>  
+                <NavLink to='/' style={{color:'#fff', textDecoration:'none'}}>
+                  <Typography
+                    variant="h6"
+                    noWrap
+                    component="div"
+                    marginRight='80px'
+                    fontSize={36}
+                    fontWeight={900}
+                    sx={{
+                      textShadow: '2px 2px 5px rgba(0,0,0,0.3), 5px 5px 70px rgba(255, 255, 255, 0.5)'
+                    }}>
+                    Spice Cast
+                  </Typography>
+                </NavLink>        
+                  <List sx={{ display: 'flex', flexDirection: 'row' }}>
+                    {['Albums', 'Podcasts', 'Events', 'RadioStations'].map((text, index) => (
+                      <NavLink to={`/${text.toLocaleLowerCase()}`} style={{color:'#fff', textDecoration:'none'}}  key={index}>
+                        <ListItem>
+                            <ListItemButton>                        
+                              <ListItemIcon>
+                                {text === 'Albums' ? <LibraryMusicIcon sx={{ color: '#fff', }} /> :
+                                  text === 'Podcasts' ? <PodcastsIcon sx={{ color: '#fff' }} /> :
+                                    text === 'Events' ? <LocalActivityIcon sx={{ color: '#fff' }} /> :
+                                      <RadioIcon sx={{ color: '#fff' }} />}
+                              </ListItemIcon>
+                              <Typography fontSize={22} fontWeight={600} sx={{ textShadow: '2px 2px 5px rgba(0,0,0,0.3), 5px 5px 70px rgba(255, 255, 255, 0.5)' }}>{text}</Typography>            
+                            </ListItemButton>
+                          </ListItem>
+                      </NavLink>
+                    ))}
+                  </List>
+            </Toolbar>
+          </AppBar>
+        </StyledDevice>
     </>
   );
 }
