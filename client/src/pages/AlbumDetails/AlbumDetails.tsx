ad6ce18dcfcb6ec33e82cd4c99c857ce8669cd61
lloydagola <lloydagola@users.noreply.github.com>
2024-05-31 23:52:26 +0300

    KAN-1: UI polish

--- client/src/pages/AlbumDetails/AlbumDetails.tsx
+++ client/src/pages/AlbumDetails/AlbumDetails.tsx
@@ -73,7 +73,7 @@ const MusicInMyMind = () => {
                     </Typography>
                 </Box>
                 <Box>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Heqb3W8Jw_E?si=vC6MY65iHzIYOvTz" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen/>
+                    <iframe style={{width:'100%'}} height="315" src="https://www.youtube.com/embed/Heqb3W8Jw_E?si=vC6MY65iHzIYOvTz" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen/>
                 </Box>
             </Grid>            
         </Grid>
