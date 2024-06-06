4976918e18b6d956d46c0303baf26e8903376577
lloydagola <lloydagola@users.noreply.github.com>
2024-06-06 22:56:04 +0300

    KAN-2-created Hero section

--- client/src/pages/PodcastDetails/PodcastDetails.tsx
+++ client/src/pages/PodcastDetails/PodcastDetails.tsx
@@ -1,188 +1,208 @@
-import Box from "@mui/material/Box";
 import React from "react";
+import Box from "@mui/material/Box";
+import MainLayout from "src/layouts/MainLayout";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import AddToPhotosIcon from "@mui/icons-material/AddToPhotos";
+import { rightDrawerWidth } from "src/utils/constants";
 
-function PodcastDetails(): JSX.Element {
+function _HeroSection(): JSX.Element {
   return (
     <Box>
-      <Box>
-        <Box>
-          <Box>
-            <img src="https://placehold.co/24x24" alt="Logo" />
-            <h1>Music</h1>
-          </Box>
-          <input type="text" placeholder="Search..." />
-          <nav className="space-y-4">
-            <a href="/images/album-1.jpg">
-              <span>home</span>
-              <span>Home</span>
-            </a>
-            <a href="/images/album-1.jpg">
-              <span>explore</span>
-              <span>Discover</span>
-            </a>
-            <a href="/images/album-1.jpg">
-              <span className="material-icons">library_music</span>
-              <span>Browse</span>
-            </a>
-            <a
-              href="/images/album-1.jpg"
-              className="flex items-center space-x-2"
-            >
-              <span>podcasts</span>
-              <span>Podcasts</span>
-              <span>new</span>
-            </a>
-            <a href="/images/album-1.jpg">
-              <span>radio</span>
-              <span>Radio</span>
-            </a>
-          </nav>
-          <Box>
-            <h2>Library: 3</h2>
-            <a href="/images/album-1.jpg">
-              <span>album</span>
-              <span>Albums</span>
-            </a>
-            <a href="/images/album-1.jpg">
-              <span>music_note</span>
-              <span>Song</span>
-            </a>
-            <a href="/images/album-1.jpg">
-              <span>person</span>
-              <span>Artists</span>
-            </a>
-          </Box>
-          <Box className="flex items-center space-x-2">
-            <img src="https://placehold.co/32x32" alt="User" />
-            <Box>
-              <p>Vitaliy Dorozhko</p>
-              <p>Premium member</p>
-            </Box>
-          </Box>
-        </Box>
+      <img
+        src="/images/hero-0.jpg"
+        alt="Artist"
+        style={{ borderRadius: "24px" }}
+      />
 
+      <Box position="absolute" zIndex={3}>
+        <p>Artist</p>
+        <h3>Top all over the world</h3>
         <Box>
-          <Box>
-            <h2>Trending</h2>
-            <Box>
-              <button>chevron_left</button>
-              <button>chevron_right</button>
-            </Box>
-          </Box>
-          <Box>
-            <img src="https://placehold.co/100x100" alt="Artist" />
-            <Box>
-              <p>Artist</p>
-              <h3>Top all over the world</h3>
-              <Box>
-                <button>Play</button>
-                <button>Follow</button>
-              </Box>
-            </Box>
-          </Box>
-          <Box>
-            <h3>Playlist</h3>
-            <table>
-              <thead>
-                <tr>
-                  <th>/images/album-1.jpg</th>
-                  <th>Title</th>
-                  <th>Artist</th>
-                  <th>Time</th>
-                  <th>Album</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>01</td>
-                  <td>Sleep 4Ever</td>
-                  <td>Blackbear</td>
-                  <td>4:12</td>
-                  <td>HoneyWorks</td>
-                </tr>
-                <tr>
-                  <td>02</td>
-                  <td>Time is Ticking Out</td>
-                  <td>The Cranberries</td>
-                  <td>4:20</td>
-                  <td>Wake up and Smell the Coffee</td>
-                </tr>
-                <tr>
-                  <td>03</td>
-                  <td>If I were u</td>
-                  <td>Lauv</td>
-                  <td>3:12</td>
-                  <td>The Ecstatic</td>
-                </tr>
-                <tr>
-                  <td>04</td>
-                  <td>One Minute More</td>
-                  <td>Gun Kelly</td>
-                  <td>2:56</td>
-                  <td>Supercell</td>
-                </tr>
-              </tbody>
-            </table>
-          </Box>
-          <Box>
-            <h3>Tags</h3>
-            <a href="/images/album-1.jpg">More</a>
-          </Box>
-          <Box>
-            <span>Accoustic</span>
-            <span>Piano jazz</span>
-            <span>Jazz</span>
-            <span>Indie pop</span>
-          </Box>
-          <Box>
-            <h3>Played</h3>
-            <a href="/images/album-1.jpg">See all</a>
-          </Box>
-          <Box className="space-y-4">
-            <Box>
-              <img src="https://placehold.co/50x50" alt="Song" />
-              <Box>
-                <p>Blank Space</p>
-                <p>Taylor Swift</p>
-                <p>4 min ago</p>
-              </Box>
-            </Box>
-            <Box>
-              <img src="https://placehold.co/50x50" alt="Song" />
-              <Box>
-                <p>Side Effects</p>
-                <p className="text-xs text-zinc-400">The Chainsmokers</p>
-                <p className="text-xs text-zinc-400">20 min ago</p>
-              </Box>
-            </Box>
-            <Box className="flex items-center space-x-4">
-              <img src="https://placehold.co/50x50" alt="Song" />
-              <Box>
-                <p>No One Like You</p>
-                <p className="text-xs text-zinc-400">Scorpions</p>
-                <p className="text-xs text-zinc-400">2 hr ago</p>
-              </Box>
-            </Box>
-            <Box className="flex items-center space-x-4">
-              <img src="https://placehold.co/50x50" alt="Song" />
-              <Box>
-                <p>Always Love You</p>
-                <p>Elton John</p>
-                <p>3 hr ago</p>
-              </Box>
-            </Box>
-          </Box>
-          <Box className="bg-purple-700 p-4 rounded-lg flex items-center space-x-4">
-            <img src="https://placehold.co/100x100" alt="Album" />
-            <Box>
-              <h3>I Knew You Were</h3>
-              <p>Taylor Swift</p>
-            </Box>
-            <button>+</button>
-          </Box>
+          <button>Play</button>
+          <button>Follow</button>
         </Box>
       </Box>
     </Box>
+  );
+}
+
+function HeroSection({
+  title = "",
+  thumbnail = "",
+}: {
+  title: string;
+  thumbnail: string;
+}): JSX.Element {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      width={1080}
+      m="auto"
+      overflow="hidden"
+      position="relative"
+    >
+      <Box
+        style={{
+          position: "absolute",
+          background: "rgba(0, 0, 0, 0.5)",
+          zIndex: 2,
+          height: "100%",
+          width: "100%",
+        }}
+      />
+      {!title || !thumbnail ? (
+        <Typography variant="h1">Loading...</Typography>
+      ) : (
+        <>
+          <img
+            src={thumbnail}
+            alt="album art"
+            style={{
+              zIndex: 0,
+              margin: "auto",
+              borderRadius: "32px",
+              width: "100%",
+            }}
+          />
+          <Box
+            position="absolute"
+            zIndex={3}
+            display="flex"
+            flexDirection="column"
+            p={{ xs: "16px", md: "64px" }}
+          >
+            <Typography
+              variant="h4"
+              zIndex={3}
+              fontSize="5rem"
+              fontWeight={600}
+            >
+              {title}
+            </Typography>
+            <Typography
+              variant="h2"
+              zIndex={3}
+              fontSize="12rem"
+              fontWeight={600}
+            >
+              {title}
+            </Typography>
+            <Box zIndex={10}>
+              <Button
+                variant="contained"
+                color="error"
+                sx={{
+                  margin: "16px",
+                  padding: "16px",
+                  width: "160px",
+                  borderRadius: "16px",
+                }}
+              >
+                <PlayArrowIcon
+                  fontSize="large"
+                  onClick={() => console.log("playing podcast...")}
+                />
+                <Typography flex={1}>Play</Typography>
+              </Button>
+              <Button
+                variant="outlined"
+                color="warning"
+                sx={{
+                  margin: "16px",
+                  padding: "16px",
+                  width: "160px",
+                  borderRadius: "16px",
+                }}
+              >
+                <AddToPhotosIcon
+                  fontSize="large"
+                  onClick={() => console.log("following podcast...")}
+                />
+                <Typography flex={1}>Follow</Typography>
+              </Button>
+            </Box>
+          </Box>
+        </>
+      )}
+    </Box>
+  );
+}
+
+function PlaylistSection(): JSX.Element {
+  return (
+    <Box>
+      <h3>Playlist</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>/images/album-1.jpg</th>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Time</th>
+            <th>Album</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>01</td>
+            <td>Sleep 4Ever</td>
+            <td>Blackbear</td>
+            <td>4:12</td>
+            <td>HoneyWorks</td>
+          </tr>
+          <tr>
+            <td>02</td>
+            <td>Time is Ticking Out</td>
+            <td>The Cranberries</td>
+            <td>4:20</td>
+            <td>Wake up and Smell the Coffee</td>
+          </tr>
+          <tr>
+            <td>03</td>
+            <td>If I were u</td>
+            <td>Lauv</td>
+            <td>3:12</td>
+            <td>The Ecstatic</td>
+          </tr>
+          <tr>
+            <td>04</td>
+            <td>One Minute More</td>
+            <td>Gun Kelly</td>
+            <td>2:56</td>
+            <td>Supercell</td>
+          </tr>
+        </tbody>
+      </table>
+    </Box>
+  );
+}
+
+function PodcastDetails(): JSX.Element {
+  return (
+    <MainLayout>
+      <Box
+        component="section"
+        sx={{
+          flexGrow: 1,
+          width: { sm: `calc(100% - ${rightDrawerWidth}px)` },
+          minHeight: "100vh",
+          marginTop: { md: 12 },
+        }}
+      >
+        <Box>
+          <Typography variant="h2" fontSize="3rem" fontWeight={600}>
+            Trending
+          </Typography>
+        </Box>
+        <HeroSection title="Hello" thumbnail="/images/hero-0.jpg" />
+        {/* <PlaylistSection /> */}
+      </Box>
+    </MainLayout>
   );
 }
 
