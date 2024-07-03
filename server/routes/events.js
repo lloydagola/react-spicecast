07421c18f733bc130d3139565ad2e7210135ea95
lloydagola <lloydagola@users.noreply.github.com>
2024-07-03 20:24:42 +0300

    KAN-22: added Events Details route

--- server/routes/events.js
+++ server/routes/events.js
@@ -8,4 +8,19 @@ router.get("/test", (request, response) => {
   response.status(200).send(events.data);
 });
 
+router.get("/test/:id", (req, res) => {
+  if (!req.params) {
+    return res.status(200).send(events.data[0]);
+  }
+  try {
+    console.log("mackenzie...", req.params.id);
+    res.status(200).send(events.data[req.params.id]);
+  } catch (error) {
+    console.log("an error occurred", error);
+    /**
+     * @todo:handle error
+     */
+  }
+});
+
 module.exports = router;
