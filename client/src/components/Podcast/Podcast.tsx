f12fc10307206f522fa6b5c51114e9290108212f
lloydagola <lloydagola@users.noreply.github.com>
2024-07-05 01:02:08 +0300

    fixed typo

--- client/src/components/Podcast/Podcast.tsx
+++ client/src/components/Podcast/Podcast.tsx
@@ -4,8 +4,6 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
-import Typography from "@mui/material/Typography";
-
 import FacebookIcon from "@mui/icons-material/Facebook";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
