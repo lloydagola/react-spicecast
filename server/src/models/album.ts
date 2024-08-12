e43c7e8d3905c0b88a465e7d415fb3f1396cf050
lloydagola <lloydagola@users.noreply.github.com>
2024-08-12 19:02:37 +0300

    KAN-26: updated albums schema

---
+++ server/src/models/album.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const albumSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  artist: { type: String, required: true },
+  tracks: [{ type: Array, default: [] }],
+  genre: { type: Array, default: ["pop"] },
+  thumbnail: { type: String, required: true, default: "images/no-image.jpg" },
+});
+
+/* 
+albumSchema.methods.findSimilar = function (callback: any) {
+  return this.model("Album").find({ genre: this.genres }, callback);
+};
+
+albumSchema.methods.getTracks = function (callback: any) {
+  return this.tracks;
+};
+ */
+export default mongoose.model("Album", albumSchema);
