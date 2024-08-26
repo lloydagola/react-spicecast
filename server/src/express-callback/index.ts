fd00a035020426444cd1c4e5cd1e65324340da61
Eric Lloyd Ochieng <31162324+lloydagola@users.noreply.github.com>
2024-08-26 23:40:25 +0300

    Dev (#52)
    * KAN-22: updated titles
    
    * KAN-22: adusted podcast width
    
    * KAN-22: fixed bug
    
    * KAN-22: updated titles
    
    * KAN-22: code cleanup
    
    * Feature/kan 22 events (#35)
    
    * KAN-22: updated titles
    
    * KAN-22: adusted podcast width
    
    * KAN-22: fixed bug
    
    * KAN-22: updated titles
    
    * KAN-22: code cleanup
    
    * KAN-22: created Events Details page
    
    * KAN-22: added Events Details route
    
    * KAN-22: updated mock data
    
    * KAN-22: code cleanup
    
    * KAN-22: implemented events details page
    
    * KAN-22: bolded text
    
    * KAN-22: code cleanup
    
    ---------
    
    Co-authored-by: lloydagola <lloydagola@users.noreply.github.com>
    
    * Feature/kan 22 events (#38)
    
    * KAN-22: updated titles
    
    * KAN-22: adusted podcast width
    
    * KAN-22: fixed bug
    
    * KAN-22: updated titles
    
    * KAN-22: code cleanup
    
    * KAN-22: created Events Details page
    
    * KAN-22: added Events Details route
    
    * KAN-22: updated mock data
    
    * KAN-22: code cleanup
    
    * KAN-22: implemented events details page
    
    * KAN-22: bolded text
    
    * KAN-22: code cleanup
    
    * ui polish
    
    * hide track duration for livestreams
    
    * updated streamUrls
    
    * rebranded
    
    * code cleanup
    
    * ui polish
    
    * code cleanup
    
    * fixed icons not toggling when episode is clicked bug
    
    * UI polish
    
    * updated right sidebar
    
    * ui polish
    
    * updated Sidebar
    
    * UI polish
    
    * switched to rounded design
    
    * ui polish
    
    * code cleanup
    
    * ui polish
    
    * ui polish
    
    * ui polish
    
    * ui polish
    
    * updated background image
    
    * set min height to prevent janky UI
    
    * ui polish
    
    * refactoring
    
    ---------
    
    Co-authored-by: lloydagola <lloydagola@users.noreply.github.com>
    
    * fixed typo
    
    * UI polish
    
    * ui polish
    
    * fixed backdrop not fully covering parent bug
    
    * configured minimum height to fix janky loading
    
    * fixed overflowing ui on home page
    
    * added component
    
    * adjusted min-height constant value
    
    * code cleanup
    
    * fixed bugs
    
    * code cleanup
    
    * KAN-59: implemented Pagination component
    
    * KAN-59: created theme
    
    * KAN-59: code cleanup
    
    * KAN-59: hide unused param
    
    * KAN-61-lazy-load pages and provide fallback component
    
    * Feature/kan 61 lazy load pages (#45)
    
    * KAN-59: implemented Pagination component
    
    * KAN-59: created theme
    
    * KAN-59: code cleanup
    
    * KAN-59: hide unused param
    
    * KAN-61-lazy-load pages and provide fallback component
    
    * code cleanup
    
    ---------
    
    Co-authored-by: lloydagola <lloydagola@users.noreply.github.com>
    
    * Feature/kan 61 lazy load pages (#47)
    
    * KAN-59: implemented Pagination component
    
    * KAN-59: created theme
    
    * KAN-59: code cleanup
    
    * KAN-59: hide unused param
    
    * KAN-61-lazy-load pages and provide fallback component
    
    * code cleanup
    
    ---------
    
    Co-authored-by: lloydagola <lloydagola@users.noreply.github.com>
    
    * initialize ts
    
    * updated folder structure
    
    * KAN-42-initial configuration
    
    * KAN-26: connect database
    
    * KAN-26: updated albums schema
    
    * KAN-26: switched to ts
    
    * KAN-26: configured json
    
    * KAN-26: refactored Albums endpoint
    
    * KAN-26: refactoring and code cleanup
    
    * KAN-26: code cleanup
    
    * KAN-26: refactored Track endpoints
    
    * KAN-26: refactored Podcast endpoints
    
    * KAN-26: implemented Radio Station endpoints
    
    * KAN-26: implemented events and radio stations endpoints
    
    * KAN-26: implemented user endpoints
    
    * code cleanup
    
    * code cleanup
    
    * refactor
    
    * UI polish
    
    * KAN-63: Updated Request Types
    
    * KAN-63-fixed deprecation warning
    
    * KAN-63-created artist entity
    
    * KAN-63: updated types
    
    * KAN-63: created artist model
    
    * KAN-63: created artist controller
    
    * KAN-63: updated artist routes
    
    * KAN-63: UI polish
    
    * KAN-63: UI polish
    
    * KAN-63: UI polish
    
    * implemented dynamic APIs for albums
    
    ---------
    
    Co-authored-by: lloydagola <lloydagola@users.noreply.github.com>

--- server/src/express-callback/index.ts
+++ server/src/express-callback/index.ts
@@ -3,14 +3,12 @@ import { Request, Response } from "express";
 type TController = (req: Request) => any;
 
 export default function makeExpressCallback(controller: TController) {
-  return (req: Request, res: Response) => {
+  return async (req: Request, res: Response) => {
     try {
-      (async function () {
-        const data = await controller(req);
+      const data = await controller(req);
 
-        res.type("json");
-        res.status(200).send({ data });
-      })();
+      res.type("json");
+      res.status(200).send({ data });
     } catch (error: any) {
       res.status(500).send({ message: "An error occurred.", error });
     }
