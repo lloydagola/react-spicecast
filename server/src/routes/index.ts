458b4206ba168388546d4c7387b37311bdca782e
lloydagola <lloydagola@users.noreply.github.com>
2024-08-26 15:21:42 +0300

    code cleanup

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,7 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req: any, res: any, next: any) {
+router.get("/", function (req: any, res: any) {
+  console.log({ req });
   res.status(200).send("hello");
 });
 
